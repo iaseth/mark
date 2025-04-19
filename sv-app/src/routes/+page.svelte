60e5794ce24f9164ce26ecc6601afad2a277596c
Ankur Seth <iankurseth@gmail.com>
2025-04-19 06:55:37 +0530

    Implemented a barebones editor

--- sv-app/src/routes/+page.svelte
+++ sv-app/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<svelte:head>
+	<title>Mark by Ankur Seth</title>
+</svelte:head>
+
+<script lang="ts">
+	import { marked } from "marked";
+	import sanitize from "sanitize-html";
+
+	let markdown = $state('# Welcome to Mark');
+	let html = $derived.by(() => {
+		const rawHtml = marked.parse(markdown);
+		const sanitizedHtml = sanitize(rawHtml);
+		return sanitizedHtml;
+	});
+</script>
+
+<section class="min-h-screen grid grid-cols-2">
+	<section>
+		{@html html}
+	</section>
+
+	<section class="">
+		<textarea class="w-full h-full" bind:value={markdown}></textarea>
+	</section>
+</section>
